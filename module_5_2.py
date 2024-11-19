2f5c36c1c258de22c14fabdd92a268b54ec94109
sntck8 <sntck88@gmail.com>
2024-11-19 19:56:53 +0700

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return title
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 3)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
